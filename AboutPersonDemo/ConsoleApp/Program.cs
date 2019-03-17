df602660c34ace1998ce8f07d109e97124c68e14
VR-Tian <377707640@qq.com>
2019-03-17 19:26:16 +0800

    修改内容：签入控制台测试

---
+++ AboutPersonDemo/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 长度问题
+            string printStr = "Pfizer Manufacturing Deutschland GmbH，Betriebsstatte Freiburg";
+            Console.WriteLine(printStr.Length);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ AboutPersonDemo/WebApplication/Controllers/TestRouteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+/// <summary>
+/// 探究关于路由机制特性使用
+/// </summary>
+namespace WebApplication.Controllers
+{
+    public class TestRouteController : ApiController
+    {
+        public string GetResult()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public string GetResult1()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
